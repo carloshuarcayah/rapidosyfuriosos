8806ac41e3bb99fae46de90e1fa8718eff78156f
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 18:29:59 -0500

    TipoDocumentoServiceImpl implementado

---
+++ src/main/java/pe/com/rapidosyfuriosos/service/impl/TipoDocumentoServiceImpl.java
@@ -0,0 +1,57 @@
+package pe.com.rapidosyfuriosos.service.impl;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.com.rapidosyfuriosos.entity.TipoDocumentoEntity;
+import pe.com.rapidosyfuriosos.repository.TipoDocumentoRepository;
+import pe.com.rapidosyfuriosos.service.ITipoDocumentoService;
+
+import java.util.List;
+
+@Service
+public class TipoDocumentoServiceImpl implements ITipoDocumentoService {
+    @Autowired
+    private TipoDocumentoRepository repositorio;
+
+    @Override
+    public List<TipoDocumentoEntity> findAll() {
+        return repositorio.findAll();
+    }
+
+    @Override
+    public List<TipoDocumentoEntity> findAllCustom() {
+        return repositorio.findAllCustom();
+    }
+
+    @Override
+    public TipoDocumentoEntity findById(Long id) {
+        return repositorio.findById(id).get();
+    }
+
+    @Override
+    public TipoDocumentoEntity add(TipoDocumentoEntity obj) {
+        return repositorio.save(obj);
+    }
+
+    @Override
+    public TipoDocumentoEntity update(TipoDocumentoEntity obj, Long id) {
+        TipoDocumentoEntity objtipd = repositorio.findById(id).get();
+        BeanUtils.copyProperties(obj, objtipd);
+        return repositorio.save(objtipd);
+    }
+
+    @Override
+    public TipoDocumentoEntity delete(Long id) {
+        TipoDocumentoEntity objtipd = repositorio.findById(id).get();
+        objtipd.setEstado(false);
+        return repositorio.save(objtipd);
+    }
+
+    @Override
+    public TipoDocumentoEntity enable(Long id) {
+        TipoDocumentoEntity objtipd = repositorio.findById(id).get();
+        objtipd.setEstado(true);
+        return repositorio.save(objtipd);
+    }
+}

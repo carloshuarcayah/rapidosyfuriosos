d37f22b6a9641d73e048d5b439222d9ee85c3742
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 13:52:06 -0500

    EmpleadoEntity implementado

---
+++ src/main/java/pe/com/rapidosyfuriosos/entity/EmpleadoEntity.java
@@ -0,0 +1,82 @@
+package pe.com.rapidosyfuriosos.entity;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import org.springframework.format.annotation.DateTimeFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity(name="EmpleadoEntity")
+@Table(name="empleado")
+public class EmpleadoEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name="codemp")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    @Column(name="nomemp", length = 40, nullable = false)
+    private String nombre;
+
+    @Column(name="apepemp", length = 40, nullable = false)
+    private String apellidopaterno;
+
+    @Column(name="apememp", length = 40, nullable = false)
+    private String apellidomaterno;
+
+    @Column(name="docemp", length = 20, nullable = false)
+    private String numerodocumento;
+
+    @Column(name="fecemp", nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate fechanacimiento;
+
+    @Column(name="diremp", length = 300, nullable = false)
+    private String direccion;
+
+    @Column(name="telemp", length = 15, nullable = false)
+    private String telefono;
+
+    @Column(name="celemp", length = 15, nullable = false)
+    private String celular;
+
+    @Column(name="coremp", length = 100, nullable = false)
+    private String correo;
+
+    @Column(name="usuemp", length = 40, nullable = false)
+    private String usuario;
+
+    @Column(name="claemp", length = 255, nullable = false)
+    private String clave;
+
+    @Column(name="estemp", nullable = false)
+    private boolean estado;
+
+    // CLAVES FORANEAS
+    @ManyToOne
+    @JoinColumn(name = "coddis", nullable = false)
+    private DistritoEntity distrito;
+
+    @ManyToOne
+    @JoinColumn(name = "codrol", nullable = false)
+    private RolEntity rol;
+
+    @ManyToOne
+    @JoinColumn(name = "codsex", nullable = false)
+    private SexoEntity sexo;
+}

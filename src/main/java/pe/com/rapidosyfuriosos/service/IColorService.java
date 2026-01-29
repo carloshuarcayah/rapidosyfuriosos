09e6403c13ab5dfa4d3baf468e41279137206a4c
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 18:13:23 -0500

    IColorService implementado

---
+++ src/main/java/pe/com/rapidosyfuriosos/service/IColorService.java
@@ -0,0 +1,14 @@
+package pe.com.rapidosyfuriosos.service;
+
+import pe.com.rapidosyfuriosos.entity.ColorEntity;
+import java.util.List;
+
+public interface IColorService {
+    List<ColorEntity> findAll();
+    List<ColorEntity> findAllCustom();
+    ColorEntity findById(Long id);
+    ColorEntity add(ColorEntity obj);
+    ColorEntity update(ColorEntity obj, Long id);
+    ColorEntity delete(Long id);
+    ColorEntity enable(Long id);
+}

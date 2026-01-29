f873c52720e05527e79796b13b1970f154404c70
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 18:38:40 -0500

    ClienteServiceImpl implementado

---
+++ src/main/java/pe/com/rapidosyfuriosos/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,57 @@
+package pe.com.rapidosyfuriosos.service.impl;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.com.rapidosyfuriosos.entity.ClienteEntity;
+import pe.com.rapidosyfuriosos.repository.ClienteRepository;
+import pe.com.rapidosyfuriosos.service.IClienteService;
+
+import java.util.List;
+
+@Service
+public class ClienteServiceImpl implements IClienteService {
+    @Autowired
+    private ClienteRepository repositorio;
+
+    @Override
+    public List<ClienteEntity> findAll() {
+        return repositorio.findAll();
+    }
+
+    @Override
+    public List<ClienteEntity> findAllCustom() {
+        return repositorio.findAllCustom();
+    }
+
+    @Override
+    public ClienteEntity findById(Long id) {
+        return repositorio.findById(id).get();
+    }
+
+    @Override
+    public ClienteEntity add(ClienteEntity obj) {
+        return repositorio.save(obj);
+    }
+
+    @Override
+    public ClienteEntity update(ClienteEntity obj, Long id) {
+        ClienteEntity objcli = repositorio.findById(id).get();
+        BeanUtils.copyProperties(obj, objcli);
+        return repositorio.save(objcli);
+    }
+
+    @Override
+    public ClienteEntity delete(Long id) {
+        ClienteEntity objcli = repositorio.findById(id).get();
+        objcli.setEstado(false);
+        return repositorio.save(objcli);
+    }
+
+    @Override
+    public ClienteEntity enable(Long id) {
+        ClienteEntity objcli = repositorio.findById(id).get();
+        objcli.setEstado(true);
+        return repositorio.save(objcli);
+    }
+}

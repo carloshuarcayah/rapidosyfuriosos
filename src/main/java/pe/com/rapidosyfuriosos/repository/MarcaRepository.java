001222c90f8dd94b44f921734a949c9460a2c971
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 14:07:39 -0500

    MarcaRepository implementado

---
+++ src/main/java/pe/com/rapidosyfuriosos/repository/MarcaRepository.java
@@ -0,0 +1,11 @@
+package pe.com.rapidosyfuriosos.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pe.com.rapidosyfuriosos.entity.MarcaEntity;
+
+public interface MarcaRepository extends JpaRepository<MarcaEntity, Long> {
+    @Query("select m from MarcaEntity m where m.estado=true")
+    List<MarcaEntity> findAllCustom();
+}

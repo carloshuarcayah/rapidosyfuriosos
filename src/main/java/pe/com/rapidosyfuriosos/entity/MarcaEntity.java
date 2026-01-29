20bf3dd93f37292ecb330a71143ad017238e6c71
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 13:43:10 -0500

    MarcaEntity implementado

---
+++ src/main/java/pe/com/rapidosyfuriosos/entity/MarcaEntity.java
@@ -0,0 +1,32 @@
+package pe.com.rapidosyfuriosos.entity;
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity(name="MarcaEntity")
+@Table(name="marca")
+public class MarcaEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Column(name="codmar")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    @Column(name="nommar", length = 50, nullable = false)
+    private String nombre;
+
+    @Column(name="estmar", nullable = false)
+    private boolean estado;
+}

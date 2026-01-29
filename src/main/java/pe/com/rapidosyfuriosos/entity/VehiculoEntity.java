110d3f238e4eaa0fc9d1659291a3cb801f37554e
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 13:55:02 -0500

    VehiculoEntity

---
+++ src/main/java/pe/com/rapidosyfuriosos/entity/VehiculoEntity.java
@@ -0,0 +1,55 @@
+package pe.com.rapidosyfuriosos.entity;
+
+import java.io.Serializable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity(name="VehiculoEntity")
+@Table(name="vehiculo")
+public class VehiculoEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name="codveh")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    @Column(name="plaveh", length = 10, nullable = false)
+    private String placa;
+
+    @Column(name="modveh", length = 50, nullable = false)
+    private String modelo;
+
+    @Column(name="anioveh", nullable = false)
+    private int anio;
+
+    @Column(name="estveh", nullable = false)
+    private boolean estado;
+
+    // CLAVES FORANEAS
+    @ManyToOne
+    @JoinColumn(name = "codmar", nullable = false)
+    private MarcaEntity marca;
+
+    @ManyToOne
+    @JoinColumn(name = "codcol", nullable = false)
+    private ColorEntity color;
+
+    @ManyToOne
+    @JoinColumn(name = "codcli", nullable = false)
+    private ClienteEntity cliente;
+}

0e50d9042f59d24da20a35e080fdd38852d368fd
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 19:10:12 -0500

    RolRestController implementado

---
+++ src/main/java/pe/com/rapidosyfuriosos/restcontroller/RolRestController.java
@@ -0,0 +1,51 @@
+package pe.com.rapidosyfuriosos.restcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.com.rapidosyfuriosos.entity.RolEntity;
+import pe.com.rapidosyfuriosos.service.IRolService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rol")
+public class RolRestController {
+
+    @Autowired
+    private IRolService servicio;
+
+    @GetMapping
+    public List<RolEntity> findAll(){
+        return servicio.findAll();
+    }
+
+    @GetMapping("/custom")
+    public List<RolEntity> findAllCustom(){
+        return servicio.findAllCustom();
+    }
+
+    @GetMapping("/{id}")
+    public RolEntity findById(@PathVariable Long id){
+        return servicio.findById(id);
+    }
+
+    @PostMapping
+    public RolEntity add(@RequestBody RolEntity obj){
+        return servicio.add(obj);
+    }
+
+    @PutMapping("/{id}")
+    public RolEntity update(@RequestBody RolEntity obj, @PathVariable Long id){
+        return servicio.update(obj, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public RolEntity delete(@PathVariable Long id){
+        return servicio.delete(id);
+    }
+
+    @PutMapping("/enable/{id}")
+    public RolEntity enable(@PathVariable Long id){
+        return servicio.enable(id);
+    }
+}

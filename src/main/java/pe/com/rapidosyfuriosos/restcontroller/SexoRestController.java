1df1e4e3fc627eee756490dfa4b37287f2ad8b4d
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 19:05:38 -0500

    SexoRestController implementado

---
+++ src/main/java/pe/com/rapidosyfuriosos/restcontroller/SexoRestController.java
@@ -0,0 +1,51 @@
+package pe.com.rapidosyfuriosos.restcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.com.rapidosyfuriosos.entity.SexoEntity;
+import pe.com.rapidosyfuriosos.service.ISexoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sexo")
+public class SexoRestController {
+
+    @Autowired
+    private ISexoService servicio;
+
+    @GetMapping
+    public List<SexoEntity> findAll(){
+        return servicio.findAll();
+    }
+
+    @GetMapping("/custom")
+    public List<SexoEntity> findAllCustom(){
+        return servicio.findAllCustom();
+    }
+
+    @GetMapping("/{id}")
+    public SexoEntity findById(@PathVariable Long id){
+        return servicio.findById(id);
+    }
+
+    @PostMapping
+    public SexoEntity add(@RequestBody SexoEntity obj){
+        return servicio.add(obj);
+    }
+
+    @PutMapping("/{id}")
+    public SexoEntity update(@RequestBody SexoEntity obj, @PathVariable Long id){
+        return servicio.update(obj, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public SexoEntity delete(@PathVariable Long id){
+        return servicio.delete(id);
+    }
+
+    @PutMapping("/enable/{id}")
+    public SexoEntity enable(@PathVariable Long id){
+        return servicio.enable(id);
+    }
+}

01c1314e5132ba5aecc04669f7e80b4d46fd9223
Carlos Huarcaya <carloshuarcayah@gmail.com>
2026-01-29 19:08:39 -0500

    TipoDocumentoRestController implementado

---
+++ src/main/java/pe/com/rapidosyfuriosos/restcontroller/TipoDocumentoRestController.java
@@ -0,0 +1,51 @@
+package pe.com.rapidosyfuriosos.restcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pe.com.rapidosyfuriosos.entity.TipoDocumentoEntity;
+import pe.com.rapidosyfuriosos.service.ITipoDocumentoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tipodocumento")
+public class TipoDocumentoRestController {
+
+    @Autowired
+    private ITipoDocumentoService servicio;
+
+    @GetMapping
+    public List<TipoDocumentoEntity> findAll(){
+        return servicio.findAll();
+    }
+
+    @GetMapping("/custom")
+    public List<TipoDocumentoEntity> findAllCustom(){
+        return servicio.findAllCustom();
+    }
+
+    @GetMapping("/{id}")
+    public TipoDocumentoEntity findById(@PathVariable Long id){
+        return servicio.findById(id);
+    }
+
+    @PostMapping
+    public TipoDocumentoEntity add(@RequestBody TipoDocumentoEntity obj){
+        return servicio.add(obj);
+    }
+
+    @PutMapping("/{id}")
+    public TipoDocumentoEntity update(@RequestBody TipoDocumentoEntity obj, @PathVariable Long id){
+        return servicio.update(obj, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public TipoDocumentoEntity delete(@PathVariable Long id){
+        return servicio.delete(id);
+    }
+
+    @PutMapping("/enable/{id}")
+    public TipoDocumentoEntity enable(@PathVariable Long id){
+        return servicio.enable(id);
+    }
+}
